dfdfc88908c5b5a9ad58b06c66eb254b706d6c66
barath <barath2112000@gmail.com>
2023-06-05 22:33:45 +0530

    branching if else

---
+++ branching & iterations.js/branch.js
@@ -0,0 +1,30 @@
+// branching -- its a decision making-- if, else//
+
+// const randomNumber = 88
+// let userGuess = 55;
+// if ( userGuess > randomNumber){ console.log('TooHigh');}
+// else { 
+//     if( userGuess< randomNumber){ console.log('TooLow');}
+// else {console.log('You are Correct');}
+
+// }
+// else if method
+// const randomNumber = 99
+// let userGuess = 105;
+// if ( userGuess > randomNumber){ console.log('TooHigh');}
+//  else if(userGuess<randomNumber) {console.log('TooLow')}
+//  else {console.log('Your correct')}
+
+// switch case
+//switch
+//case-->response
+
+const weekday = prompt('what day is today?  ');
+
+
+// if (weekday == 'Monday')
+// {console.log('Today is Monday');}
+// else if (weekday == 'Tuesday') {console.log('Today is Tuesday');}
+// else if (weekday = = 'Wednesday') {console.log('Today is Wednesday');}
+// else if (weekday == 'Thursday'); {console.log('Today is Thursday');}
+
