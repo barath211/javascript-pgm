de32c5a1f3507df4e1477dacfb8fa4653f03e1d3
barath <barath2112000@gmail.com>
2023-06-09 19:13:10 +0530

    data structures ,index,pop& shift method length

---
+++ arrays.js/arrays.js
@@ -0,0 +1,70 @@
+
+// -- DATA STRUCTURE--//
+
+// *1 ARRAY
+// *2 OBJECT
+// THESE  TWO BELONGS TO PRIMITIVES //
+
+
+// ARRAY--> LIST,ORDERED LIST'
+// HOW TO DECLARE A Array
+
+// 1-ARRAY CONSTRUCTOR
+// const numbers = new Array ();
+// console.log(numbers); 
+// console.log(typeof numbers);
+
+
+// 2-LITERAL CREATION 
+// const numbers =[1,2,3,4,5];
+// console.log(numbers);
+// console.table(numbers);
+
+//  const names = ['barath saran raghul mervin'];
+//   console.log(names);
+//   console.table(names);
+
+// -- how to modify the array elements --// by using ( index )
+
+//  const numbers = [1,2,3,4,5];
+//  console.log(numbers);
+// console.log(numbers[2]);
+//  console.log(numbers[0]);
+// console.log(numbers[3]);
+// console.log(numbers[1]);
+// console.log(numbers[4]);
+ 
+//--how to modify the arrays//
+// const numbers= [1,2,3,4,5];
+
+// console.log(numbers);
+// numbers[2] = 8;
+// console.log(numbers);
+
+//array length //
+// const fruits = ['orange', 'apple', 'grapes', 'banana', 'mango'];
+// const webpage = ['html', 'css', 'javascript','bootstrap', 'mongodb','reactjs'];
+ //  console.log('no of fruits:', fruits.length);
+// console.log('no of webpages:', webpage.length);
+
+// how to find middle index of an Array
+// const MiddleIndex = fruits.length /2;
+// console.log(MiddleIndex);
+// console.log(fruits [MiddleIndex]);
+
+
+// const LastIndex = fruits.length-1;
+// console.log(fruits[LastIndex])
+// console.log(LastIndex[fruits.length]);
+
+// HOW TO MODIFY ARRAYS --> it has used for removing last element of array//
+// POP METHOD //
+// const numbers = [0.1,0.2,0.3,0.4,0.5];
+// console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
+
+// shift method --> it is used for removing 1st element of array //
+// console.log(numbers);
+// numbers.shift();
+// console.log(numbers);
