f90d06ddae7d017faec02f12892d14b381260cd5
barath <barath2112000@gmail.com>
2023-06-19 10:30:46 +0530

    slice splice index sum of array

--- arrays.js/arrays.js
+++ arrays.js/arrays.js
@@ -68,3 +68,81 @@
 // console.log(numbers);
 // numbers.shift();
 // console.log(numbers);
+
+
+ //  slice method --> it is used for copy a part of the array //
+  
+//  const webpage = ['html', 'css', 'javascript','bootstrap', 'mongodb','reactjs'];
+//  console.log(webpage);
+
+//  const frontend = webpage.slice(0,3);// one way to copying array// 
+//  console.log(frontend);
+//  // if we need all the elements give webpage.slice(0); then it will automatically convert to length
+//  //ex ['html', 'css', 'javascript','bootstrap', 'mongodb','reactjs']; like this
+// const BackEnd = webpage.slice(-3,);// if we give minus value it start from last value//
+// console.log(BackEnd);
+
+//splice method it is used for removing a element //((doubt))
+// const bikes = [ 'yamaha', 'bajaj', 'ninja', 'honda', ];
+// console.log('bikes');
+// console.log(bikes);
+// // 1st is index num, next is how elements want to delete , next one is add in string//
+// bikes.splice(0,2,...['suzuki', 'yamaha', 'bajaj',], );
+// console.log(bikes);
+
+
+// //split method --> this used for create an array from strings -- //
+
+// const languages = 'english';
+//  const mylanguages = languages.split();
+// console.log(mylanguages);
+
+// const sentence = 'Wikipedia differs from printed references in important ways. It is continually created and updated and encyclopedic articles on new events appear within minutes rather than months or years.'
+// // ('') IS SPLIT EVERY ELEMENT AS A STRING -->
+// //   ANOTHER ONE IS (' ') THIS IS WHERE GIVEN SPACE TAKE AS A ARRAY
+// // ('a') is used for how many 'a' used in a given string
+// console.log(sentence.split(' '));
+
+// join method  is convert a array of character to words
+// const brr = ['b','a','r','a','t','h'];
+// console.log(brr);
+// console.log(brr.join(''));
+
+// reverse method 
+// const numbers = [1,2,3,4,5];
+// console.log(numbers.reverse())
+
+// indexof method--> 
+// it  is used for find the index value
+// const marks = [100,110,120,130,130,140,150];
+// console.log(marks.indexOf(130));
+// console.log(marks.lastIndexOf(130));// when a element is repeat again if we want second value of index then we can use lastindexof
+
+// -- Loops of array -- //
+// const socialMedia = [ 'instagram', 'facebook', 'whatsapp', 'snap', 'youtube',]; 
+
+// for(let s=0; s<5; s += 1){ console.log(socialMedia[s]);
+
+// }
+
+// how to find sum of array //
+// const arr =[ 20,25,30,10,5,89,45,74,26,38,96,64,58,52];
+// let sum = 0;
+// for( let a=0; a<arr.length; a += 1 )
+//  { sum = sum + arr[a]}
+//    //sum  = 0 + 20  ==20
+//    //sum  = 20 + 25  == 45
+
+// console.log(sum);
+ 
+
+// how to find odd number // 
+// const number = [1,2,3,4,5,6,7,8,9,10];
+//  const oddNumbers = [];
+// for( let i = 0; i<number.length; i += 1) { 
+//   if (number[i] % 2 === 1)
+//   {
+//  oddNumbers.push(number[i]);
+//   }
+// }
+// console.log(oddNumbers);
